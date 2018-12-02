27909bb8036bdc4a8b582f63452bdadcf6fa8c25
David Bess <davidbess@Davids-MacBook-Pro.local>
2018-12-02 16:24:32 -0700

    POJO

---
+++ src/main/java/edu/jhu/clueless/domain/Weapon.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package edu.jhu.clueless.domain;
+
+/**
+ * @author davidbess
+ *
+ */
+public class Weapon
+{
+  String name;
+
+  /**
+   * @return the name
+   */
+  public String getName()
+  {
+    return name;
+  }
+
+  /**
+   * @param name the name to set
+   */
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+  
+  
+  
+}
