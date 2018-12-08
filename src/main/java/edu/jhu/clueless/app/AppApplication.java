9a55478d3a88d09a3b34e961d9885b7376b1ca69
tom <nf11624@gmail.com>
2018-12-08 12:46:29 -0800

    Updates for buildw

--- src/main/java/edu/jhu/clueless/app/AppApplication.java
+++ src/main/java/edu/jhu/clueless/app/AppApplication.java
@@ -1,9 +1,14 @@
 package edu.jhu.clueless.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude ={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class AppApplication {
 
 	public static void main(String[] args) {
