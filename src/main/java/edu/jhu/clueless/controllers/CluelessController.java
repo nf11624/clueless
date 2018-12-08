e51692941a09b2592a22dee4946f6e4cd0f8315f
David Bess <davidbess@Davids-MacBook-Pro.local>
2018-12-08 14:31:06 -0700

    small changes

--- src/main/java/edu/jhu/clueless/controllers/CluelessController.java
+++ src/main/java/edu/jhu/clueless/controllers/CluelessController.java
@@ -3,11 +3,46 @@
  */
 package edu.jhu.clueless.controllers;
 
-/**
- * @author davidbess
- *
- */
-public class CluelessController
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.jhu.clueless.DTO.InitRequestDTO;
+import edu.jhu.clueless.DTO.InitResponseDTO;
+import edu.jhu.clueless.service.CluelessService;
+
+@RestController
+@RequestMapping("${application.base-path}/api")
+public class CluelessController 
 {
-  
+    private final CluelessService myCluelessService;
+    
+     
+
+    public CluelessController(CluelessService myCluelessService)
+    {
+      this.myCluelessService = myCluelessService;
+    }
+
+
+
+    /**
+     * 
+     * @param myInitRequestDTO
+     * @return
+     * @throws IOException
+     * @throws JSONException
+     */
+    @PostMapping("/init")
+    ResponseEntity<InitResponseDTO> getDocumentsWithBiasingBOQ(@RequestBody InitRequestDTO myInitRequestDTO) throws IOException, JSONException 
+    {
+      InitResponseDTO result = myCluelessService.initGame(myInitRequestDTO);
+      return ResponseEntity.ok().body(result);
+    }
+
 }
